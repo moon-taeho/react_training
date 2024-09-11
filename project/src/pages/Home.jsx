87c9411d0ce2cd56893505bfb2f4eedec8284636
moon taeho <moon.taeho@mic-p.com>
2024-09-11 08:12:01 +0000

    1日目（課題済み）

--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Button, Input, Typography } from '@mui/material';
 
 const Home = () => {
-	return <div>Home</div>;
+	const [a, setA] = useState(0);
+	const [b, setB] = useState(0);
+	const [result, setResult] = useState(0);
+
+	const handleClick = () => {
+		setResult(a + b);
+	};
+
+	return (
+		<Box
+			sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 1, p: 1 }}
+		>
+			<Input onChange={(e) => setA(Number(e.target.value))} />+
+			<Input onChange={(e) => setB(Number(e.target.value))} />
+			<Button variant="contained" onClick={handleClick}>
+				=
+			</Button>
+			<Typography>{result}</Typography>
+		</Box>
+	);
 };
 
 export default Home;
